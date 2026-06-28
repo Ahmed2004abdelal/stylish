34aa599df1e116a29ad7d59ed954b7bc0b85a352
Ahmed_Abdelaal <abdelalahmed030@gmail.com>
2026-06-28 23:27:10 +0300

    docs: add professional README with screenshots

--- lib/core/utilities/cosntant.dart
+++ lib/core/utilities/cosntant.dart
@@ -1,7 +1,4 @@
 class Constants {
   static const paymentIntentPath = "https://api.stripe.com/v1/payment_intents";
-  static const paymentPublishableKey =
-      "pk_test_51TlpUBRz8VD2u8oKlykanxLTZirDhmJlIfmH8fx6ZDPbI9hePOEwodBQjox49x4yZCWtG2L6ymKEyGWR2FaE2fDF00i2zKkoEK";
-  static const paymentSecretKey =
-      "sk_test_51TlpUBRz8VD2u8oK0hNugLBGkWgQ3sFpdOAUAJArU4dUMOVdjG1urKCPPlsLb02wTZvg9cBtDncVOTqVi7c40y73009CdxBetp";
+
 }
